2c5499e70282c415c470a331c0aeee4089da36f8
a.cornish <a.cornish@bc3139a8-67e5-0310-9ffc-ced21a209358>
2014-05-18 16:30:57 +0000

    Shuffle function and test added
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/SANTA@90452 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/Shuffle.R
@@ -0,0 +1,8 @@
+Shuffle <- function(x, ignore=NULL) {
+    # shuffle the values in x
+    # ignore should be a logical vector equal in length to x
+    # for each T value in ignore, the corresponding value in x is not shuffled
+    if (is.null(ignore)) ignore <- rep(F, length(x))
+    x[!ignore] <- sample(x[!ignore])
+    x    
+}
